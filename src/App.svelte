10d4feec46e9a6b5eafe0866ed0106a333875e73
Etim Justine <etimjustine@Etims-MacBook-Pro.local>
2024-11-14 00:53:15 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+import Sliider from "./Sliider.svelte";
+let redColor = 100;
+let greenColor = 0;
+let blueColor = 0;
+</script>
+<div class="row">
+  <div class="col">
+    <h1 style="color: rgb({redColor}, {greenColor}, {blueColor});">KEDUKA- JUST COLOR YOUR WORLD</h1>
+  </div>
+</div>
+<div class='row'>
+<div class="column">
+  <form>
+    <Sliider colorName="Red" bind:colorValue={redColor}/>
+    <Sliider colorName="Green" bind:colorValue={greenColor}/>
+    <Sliider colorName="Blue" bind:colorValue={blueColor}/>
+  </form>
+</div>
+</div>
+<div class="row">
+  <div class="column">
+    <div id="color" style="background-color: rgb({redColor}, {blueColor}, {greenColor});"></div>
+  </div>
+  <div class="row">
+    <div class="column">
+      <h2>rgb({redColor}, {blueColor}, {greenColor})</h2>
+    </div>
+  </div>
+</div>
+<style>
+  #color{
+    height: 300px;
+    border: dashed 5px;
+  }
+  h2{
+    text-align: center;
+    margin-top: 20px;
+  }
+ 
+  
+</style>
---
+++ src/Sliider.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let colorName;
+  export let colorValue;
+</script>
+<label for={colorName}>{colorName} ({colorValue})</label>
+    <input type="range" id= {colorValue} bind:value={colorValue} min="0" max="255">
+<style>
+  input{
+    width:100%;
+  }
+</style>
